1a7f02f1e20511aa4268734f948250470fb781a1
line2nine <hailinh203@gmail.com>
2020-04-06 09:22:40 +0700

    Add files

---
+++ errorlog.php
@@ -0,0 +1,29 @@
+[Thu Apr 02 06:22:16.699102 2020] [mpm_prefork:notice] [pid 5713] AH00163: Apache/2.4.29 (Ubuntu) configured -- resuming normal operations
+[Thu Apr 02 06:22:16.699137 2020] [core:notice] [pid 5713] AH00094: Command line: '/usr/sbin/apache2'
+[Thu Apr 02 06:23:10.642142 2020] [mpm_prefork:notice] [pid 5713] AH00169: caught SIGTERM, shutting down
+[Fri Apr 03 06:53:59.643513 2020] [mpm_prefork:notice] [pid 1155] AH00163: Apache/2.4.29 (Ubuntu) configured -- resuming normal operations
+[Fri Apr 03 06:53:59.652537 2020] [core:notice] [pid 1155] AH00094: Command line: '/usr/sbin/apache2'
+[Fri Apr 03 07:30:55.379954 2020] [php7:notice] [pid 3074] [client ::1:36716] PHP Notice:  Undefined index: investment in /var/www/html/php-future-value-calculator/result-page.php on line 15
+[Fri Apr 03 07:30:55.379986 2020] [php7:notice] [pid 3074] [client ::1:36716] PHP Notice:  Undefined index: rate in /var/www/html/php-future-value-calculator/result-page.php on line 16
+[Fri Apr 03 07:30:55.379995 2020] [php7:notice] [pid 3074] [client ::1:36716] PHP Notice:  Undefined index: year in /var/www/html/php-future-value-calculator/result-page.php on line 17
+[Fri Apr 03 07:32:00.831568 2020] [php7:notice] [pid 1186] [client ::1:36726] PHP Notice:  Undefined index: investment in /var/www/html/php-future-value-calculator/result-page.php on line 15, referer: http://localhost/php-future-value-calculator/result-page.php
+[Fri Apr 03 07:32:00.831612 2020] [php7:notice] [pid 1186] [client ::1:36726] PHP Notice:  Undefined index: rate in /var/www/html/php-future-value-calculator/result-page.php on line 16, referer: http://localhost/php-future-value-calculator/result-page.php
+[Fri Apr 03 07:32:00.831623 2020] [php7:notice] [pid 1186] [client ::1:36726] PHP Notice:  Undefined index: year in /var/www/html/php-future-value-calculator/result-page.php on line 17, referer: http://localhost/php-future-value-calculator/result-page.php
+[Fri Apr 03 07:32:01.831001 2020] [php7:notice] [pid 1186] [client ::1:36726] PHP Notice:  Undefined index: investment in /var/www/html/php-future-value-calculator/result-page.php on line 15, referer: http://localhost/php-future-value-calculator/result-page.php
+[Fri Apr 03 07:32:01.831025 2020] [php7:notice] [pid 1186] [client ::1:36726] PHP Notice:  Undefined index: rate in /var/www/html/php-future-value-calculator/result-page.php on line 16, referer: http://localhost/php-future-value-calculator/result-page.php
+[Fri Apr 03 07:32:01.831044 2020] [php7:notice] [pid 1186] [client ::1:36726] PHP Notice:  Undefined index: year in /var/www/html/php-future-value-calculator/result-page.php on line 17, referer: http://localhost/php-future-value-calculator/result-page.php
+[Fri Apr 03 07:35:20.769620 2020] [php7:emerg] [pid 1185] [client ::1:36744] PHP Parse error:  syntax error, unexpected '}' in /var/www/html/php-future-value-calculator/result-page.php on line 22, referer: http://localhost/php-future-value-calculator/
+[Fri Apr 03 07:39:07.463295 2020] [php7:notice] [pid 1186] [client ::1:36762] PHP Notice:  Undefined index: investment in /var/www/html/php-future-value-calculator/result-page.php on line 15
+[Fri Apr 03 07:39:07.463325 2020] [php7:notice] [pid 1186] [client ::1:36762] PHP Notice:  Undefined index: rate in /var/www/html/php-future-value-calculator/result-page.php on line 16
+[Fri Apr 03 07:39:07.463334 2020] [php7:notice] [pid 1186] [client ::1:36762] PHP Notice:  Undefined index: year in /var/www/html/php-future-value-calculator/result-page.php on line 17
+[Fri Apr 03 21:15:23.962287 2020] [mpm_prefork:notice] [pid 1155] AH00169: caught SIGTERM, shutting down
+[Mon Apr 06 07:34:41.618834 2020] [mpm_prefork:notice] [pid 1171] AH00163: Apache/2.4.29 (Ubuntu) configured -- resuming normal operations
+[Mon Apr 06 07:34:41.630221 2020] [core:notice] [pid 1171] AH00094: Command line: '/usr/sbin/apache2'
+[Mon Apr 06 08:19:32.877001 2020] [php7:notice] [pid 1192] [client ::1:50726] PHP Notice:  Undefined index: anh in /var/www/html/php-customer-filter/index.php on line 43
+[Mon Apr 06 08:19:32.877042 2020] [php7:notice] [pid 1192] [client ::1:50726] PHP Notice:  Undefined index: anh in /var/www/html/php-customer-filter/index.php on line 43
+[Mon Apr 06 08:19:32.877065 2020] [php7:notice] [pid 1192] [client ::1:50726] PHP Notice:  Undefined index: anh in /var/www/html/php-customer-filter/index.php on line 43
+[Mon Apr 06 08:19:32.877073 2020] [php7:notice] [pid 1192] [client ::1:50726] PHP Notice:  Undefined index: anh in /var/www/html/php-customer-filter/index.php on line 43
+[Mon Apr 06 08:23:38.122435 2020] [php7:notice] [pid 1188] [client ::1:50754] PHP Notice:  Undefined index: anh in /var/www/html/php-customer-filter/index.php on line 43
+[Mon Apr 06 08:23:38.122465 2020] [php7:notice] [pid 1188] [client ::1:50754] PHP Notice:  Undefined index: anh in /var/www/html/php-customer-filter/index.php on line 43
+[Mon Apr 06 08:23:38.122475 2020] [php7:notice] [pid 1188] [client ::1:50754] PHP Notice:  Undefined index: anh in /var/www/html/php-customer-filter/index.php on line 43
+[Mon Apr 06 08:23:38.122483 2020] [php7:notice] [pid 1188] [client ::1:50754] PHP Notice:  Undefined index: anh in /var/www/html/php-customer-filter/index.php on line 43
